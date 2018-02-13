a6ac31a5f8761fbba4e226f964e60a6ec52f4cdb
aimashahislam <aima.shah@square63.com>
2018-02-13 11:24:42 +0500

    User: add profile picture upload and edit functionality.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ApplicationRecord
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  
+  # has_attached_file :avatar, 
+  # :path => ":rails_root/public/system/:attachment/:id/:basename_:style.:extension",
+  # :url => "/system/:attachment/:id/:basename_:style.:extension",
+  # :styles => {
+  #   :thumb    => ['100x100#',  :jpg, :quality => 70],
+  #   :preview  => ['480x480#',  :jpg, :quality => 70],
+  #   :large    => ['600>',      :jpg, :quality => 70],
+  #   :retina   => ['1200>',     :jpg, :quality => 30]
+  # },
+  # :convert_options => {
+  #   :thumb    => '-set colorspace sRGB -strip',
+  #   :preview  => '-set colorspace sRGB -strip',
+  #   :large    => '-set colorspace sRGB -strip',
+  #   :retina   => '-set colorspace sRGB -strip -sharpen 0x0.5'
+  # }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
