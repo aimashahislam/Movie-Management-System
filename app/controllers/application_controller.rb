f121dba714d84f45f9fa1097d5d93bbd72473be8
aimashahislam <aima.shah@square63.com>
2018-02-13 11:25:44 +0500

    Bug fix: permitting parameters for User:update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_no, :gender, :date_of_birth, :status])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_no, :gender, :date_of_birth, :status, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone_no, :gender, :date_of_birth, :status, :avatar])
   end
 end
