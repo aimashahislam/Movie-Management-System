12eef2347d22e41afef3960f9d4c3b94a87af687
aimashahislam <aima.shah@square63.com>
2018-02-14 12:00:37 +0500

    Setting up admin panel configurations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
+  def after_sign_in_path_for(resource)
+    # Here you can write logic based on roles to return different after sign in paths
+    if current_user.admin == true
+      admin_movies_path
+    else
+      root_path
+    end
+  end
+  
+  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_no, :gender, :date_of_birth, :status, :avatar])
