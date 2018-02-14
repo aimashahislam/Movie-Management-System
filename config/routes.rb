3b034c9c81c2ac24cef70f62af3ca18d8751011a
aimashahislam <aima.shah@square63.com>
2018-02-14 12:08:31 +0500

    Movie:Controllers and views generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'rake/routes'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'home#index'
+
+  namespace :admin do
+    resources :movies
+  end
 
 end
