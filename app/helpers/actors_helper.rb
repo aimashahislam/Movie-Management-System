f9a2bb148e03bf79a341aada4cc3353da9768cbe
aimashahislam <aima.shah@square63.com>
2018-02-25 03:22:39 +0500

    Setting avatar helper added in actor and movie.

--- app/helpers/actors_helper.rb
+++ app/helpers/actors_helper.rb
@@ -1,2 +1,9 @@
 module ActorsHelper
+  def setActor(actor)
+    if actor.avatar.present?
+      actor.try(:avatar).try(:url)
+    else
+      "/assets/user.png"
+    end
+  end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+  def setPoster(movie)
+    if movie.posters.present?
+      movie.posters.first.try(:avatar).try(:url)
+    else
+      "/assets/default.jpeg"
+    end
+  end
 end
