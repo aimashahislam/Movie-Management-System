b8a2cf9f7cca14db9ce09ada3df8a24ca8a0bebc
aimashahislam <aima.shah@square63.com>
2018-02-26 06:47:09 +0500

    Review helper added.

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,13 @@
 module ReviewsHelper
+  def setEditLinkForReview(review)
+    if user_signed_in? && review.user_id == current_user.id
+      link_to 'Edit', edit_movie_review_path(@movie, review)
+    end
+  end
+
+  def setDeleteLinkForReview(review)
+    if user_signed_in? && review.user_id == current_user.id || current_user.try(:admin)
+      link_to 'Delete', movie_review_path(@movie, review), method: :delete, data: {confirm: "Are you sure?"}
+    end
+  end
 end
