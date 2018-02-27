1f0e514851dfc3ba16ecd4879f85da7ad1398fb8
aimashahislam <aima.shah@square63.com>
2018-02-27 12:43:05 +0500

    Bug fix: helper names changeg.

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,17 +1,17 @@
 module ReviewsHelper
-  def set_edit_link_for_review(review)
+  def edit_link_for_review(review)
     if user_signed_in? && review.user_id == current_user.id
       link_to 'Edit', edit_movie_review_path(@movie, review)
     end
   end
 
-  def set_delete_link_for_review(review)
+  def delete_link_for_review(review)
     if user_signed_in? && review.user_id == current_user.id || current_user.try(:admin)
       link_to 'Delete', movie_review_path(@movie, review), method: :delete, data: {confirm: "Are you sure?"}
     end
   end
 
-  def set_report_link_for_review(review)
+  def report_link_for_review(review)
     if user_signed_in?
       link_to 'Report', new_review_report_path(review)
     end
