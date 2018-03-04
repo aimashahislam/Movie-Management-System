78371ebcd84283c167dbc27191bd6ddcca1b2efe
aimashahislam <aima.shah@square63.com>
2018-03-05 01:13:29 +0500

    Actor: Model level validation updated.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,7 +6,7 @@ class Actor < ApplicationRecord
   has_and_belongs_to_many :movies
 
   validates :name, length: { in: 2..255 }
-  validates :bio, :name, presence: true
+  validates :bio, :name, :date_of_birth, presence: true
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
